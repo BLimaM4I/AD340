6947e17902b300f7afe8cbad1c821501b1ccee33
BLimaM4I <geral@m4i.pt>
2020-12-30 15:58:07 +0000

    add the first three elements in the activity

--- app/src/main/java/com/m4i/ad340/MainActivity.kt
+++ app/src/main/java/com/m4i/ad340/MainActivity.kt
@@ -2,10 +2,43 @@ package com.m4i.ad340
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    // region of setup methods
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val zipCodeEditText : EditText =  findViewById(R.id.zipCodeEditText)
+        val enterButton : Button = findViewById(R.id.ButtonSubmitZipCode)
+        enterButton.setOnClickListener {
+            Toast.makeText(this,"Amo-te muito. Diverte-te hoje com a Blenda. Sem segundas intenções da minha parte :)))",Toast.LENGTH_SHORT).show()
+        }
     }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+    // endregion of setup methods
+
+    // region teardown methods
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    // endregion teardown methods
+
 }
