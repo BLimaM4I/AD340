034b00799bfec6058ab7036ab4961802439fbe5c
BLimaM4I <geral@m4i.pt>
2021-01-07 17:50:42 +0000

    Completed the week 3

---
+++ app/src/main/java/com/m4i/ad340/DailyForecast.kt
@@ -0,0 +1,6 @@
+package com.m4i.ad340
+
+data class DailyForecast(
+    val temp: Float,
+    val description: String
+)
--- app/src/main/java/com/m4i/ad340/MainActivity.kt
+++ app/src/main/java/com/m4i/ad340/MainActivity.kt
@@ -1,44 +1,57 @@
 package com.m4i.ad340
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
+    private val forecastRepository = ForecastRepository()
+
     // region of setup methods
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val zipCodeEditText: EditText = findViewById(R.id.zipCodeEditText)
 
-        val zipCodeEditText : EditText =  findViewById(R.id.zipCodeEditText)
-        val enterButton : Button = findViewById(R.id.ButtonSubmitZipCode)
+        //Example of button click event
+        val enterButton: Button = findViewById(R.id.ButtonSubmitZipCode)
         enterButton.setOnClickListener {
-            Toast.makeText(this,"Amo-te muito. Diverte-te hoje com a Blenda. Sem segundas intenções da minha parte :)))",Toast.LENGTH_SHORT).show()
+
+            val zipCode: String = zipCodeEditText.text.toString()
+
+            if (zipCode.length != 5) {
+                Toast.makeText(this, R.string.zipcode_message_error, Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, zipCode, Toast.LENGTH_SHORT).show()
+                forecastRepository.loadForecast(zipCode)
+            }
         }
+
+        val forecastList: RecyclerView = findViewById(R.id.forecastList)
+        forecastList.layoutManager = LinearLayoutManager(this)
+        val dailyForecastAdapter = DailyForecastAdapter() { forecastItem ->
+            val msg = getString(
+                R.string.forecast_clicked_format,
+                forecastItem.temp,
+                forecastItem.description
+            )
+            Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+        }
+        forecastList.adapter = dailyForecastAdapter
+
+        val weeklyForecastObserver = Observer<List<DailyForecast>> { forecastItems ->
+            dailyForecastAdapter.submitList(forecastItems)
+            //Toast created just to see if we can see on the output
+            //Toast.makeText(this, "Loaded itens", Toast.LENGTH_LONG).show()
+
+        }
+        forecastRepository.weeklyForecast.observe(this, weeklyForecastObserver)
     }
-
-    override fun onStart() {
-        super.onStart()
-    }
-
-    override fun onResume() {
-        super.onResume()
-    }
-    // endregion of setup methods
-
-    // region teardown methods
-
-    override fun onPause() {
-        super.onPause()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-    }
-
-    // endregion teardown methods
-
 }
